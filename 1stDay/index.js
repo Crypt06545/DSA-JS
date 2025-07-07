31831b3b54137b006aa2715981ab634c57b67a7c
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-07-07 13:04:31 +0600

    explore about type coercion

---
+++ 1stDay/index.js
@@ -0,0 +1,14 @@
+// type Coercion
+// const value1 = "5";
+// const value2 = 9;
+// let sum = value1 + value2;
+
+// console.log(sum);
+// sum = Number(value1) + value2;
+/**
+ * Type coercion is the automatic or implicit conversion of values from
+ * one data type to another (such as strings to numbers). Type conversion
+ * is similar to type coercion because they both convert values from one data
+ * type to another with one key difference — type coercion is implicit whereas type conversion can be
+ * either implicit or explicit.
+ */
