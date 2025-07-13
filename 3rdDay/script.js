db559e886141fb40739d1a1eb67f4ad09be94d4f
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-07-13 20:39:14 +0600

    explore factorial and factor

--- 3rdDay/script.js
+++ 3rdDay/script.js
@@ -28,17 +28,51 @@ Q. Guess the number
 // }
 
 // Sum of n natural numbers
+// let n = Number(prompt("Enter Your Number"));
+
+// if (isNaN(n)) {
+//   console.log("Invalid Input");
+// } else {
+//   if (n > 0) {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//       sum += i;
+//     }
+//     console.log(sum);
+//   } else {
+//     console.log("NUmber should be positive");
+//   }
+// }
+
+// factorial of n numbers
+// let n = Number(prompt("Enter Your Number"));
+
+// if (isNaN(n)) {
+//   console.log("Invalid Input");
+// } else {
+//   if (n > 0) {
+//     let fact = 1;
+//     for (let i = 1; i <= n; i++) {
+//       fact *= i;
+//     }
+//     console.log(fact);
+//   } else {
+//     console.log("NUmber should be positive");
+//   }
+// }
 let n = Number(prompt("Enter Your Number"));
 
 if (isNaN(n)) {
   console.log("Invalid Input");
 } else {
   if (n > 0) {
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-      sum += i;
+    for (let i = 1; i <= Math.floor(n / 2); i++) {
+      if (n % i === 0) {
+        console.log(i);   
+      }
     }
-    console.log(sum);
+    console.log(n);
+    
   } else {
     console.log("NUmber should be positive");
   }
