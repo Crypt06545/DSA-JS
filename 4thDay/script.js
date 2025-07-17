760f7166651853b5f3981d5c8b6ef9b4722315e5
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-07-17 14:22:34 +0600

    explore pattern programming

--- 4thDay/script.js
+++ 4thDay/script.js
@@ -6,9 +6,37 @@ import promptSync from "prompt-sync"; //--> to take input on terrminal
 const prompt = promptSync();
 const number = Number(prompt("Enter your Number :"));
 
+// base prattern
+// for (let i = 1; i <= number; i++) {
+//   // console.log(i);
+
+//   for (let j = 1; j <= number; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+// right angel traingle
+// for (let i = 1; i <= number; i++) {
+//   //   console.log(i, "*");
+//   for (let j = 1; j <= i; j++) {
+//     // process.stdout.write("* ");
+//     process.stdout.write(j + " ");
+//     // console.log(j,i);
+//   }
+//   console.log();
+// }
+
+
+// alphabet angel traingle
 for (let i = 1; i <= number; i++) {
-  for (let j = 1; j <= number; j++) {
-    process.stdout.write("* ");
+  //   console.log(i, "*");
+  let asci = 65
+  for (let j = 1; j <= i; j++) {
+    // process.stdout.write("* ");
+    process.stdout.write(String.fromCharCode(asci) + " ");
+    asci++
+    // console.log(j,i);
   }
   console.log();
 }
