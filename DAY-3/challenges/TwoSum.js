cae568e9dc4c7087f48128ba9e3662f5b4cf6237
Crypt06545 <mehedihasansagor2005.com>
2025-09-30 13:16:02 +0600

    solve array sum problem

---
+++ DAY-3/challenges/TwoSum.js
@@ -0,0 +1,14 @@
+const twoSum = (list, tarNum) => {
+  //   console.log(list, tarNum);
+  for (let i = 0; i < list.length; i++) {
+    // console.log(list[i]);
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[i] + list[j] === tarNum) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+};
+
+console.log(twoSum([2, 3, 5, 6, 8, 9, 7, 1], 10));
